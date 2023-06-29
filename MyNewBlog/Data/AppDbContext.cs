27ff6940020b1140e438e8fe3a34ff4d2acad51a
tsurubaso <tsurubaso@gmail.com>
2023-06-29 17:12:58 +0900

    Add project files.

---
+++ MyNewBlog/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace MyNewBlog.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            options.UseSqlServer(Configuration.GetConnectionString("DataBaseLink"));
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+
+
+
+    }
+
+
+}
+
+
+
+
+    
+
---
+++ MyNewBlog/Data/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace MyNewBlog.Data
+{
+    public class Post
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Introduction { get; set; }
+        public string BodyText { get; set; }
+
+    }
+}
